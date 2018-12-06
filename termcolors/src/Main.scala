97e3bd70f673edec8f9674773f8cd2d14db53ee7
Rolandas Valteris <rolandas.valteris@gmail.com>
2018-12-06 17:16:42 -0500

    Added support for named ANSI colors

--- termcolors/src/Main.scala
+++ termcolors/src/Main.scala
@@ -1,6 +1,21 @@
 object Main {
+  private val baseColors =
+    List("Black", "Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "White")
+  private val ansiColors = baseColors ++ baseColors.map("Bright" + _)
+
+  private val namedColors = ansiColors.indices.zip(ansiColors).toMap
+  private val maxColorNameLen = ansiColors.map(_.length).max
+  private val colorSpacing = 1
+
   def main(args: Array[String]): Unit = {
-    val colorMapStr = (0 to 255).map(i => fansi.Color.Full(i)(f"colour$i%-3s ")).grouped(16).map(_.mkString).mkString("\n")
+    val colorMapStr = (0 to 255)
+      .map { i =>
+        val color = namedColors.getOrElse(i, "colour" + i).padTo(maxColorNameLen + colorSpacing, ' ')
+        fansi.Color.Full(i)(color)
+      }
+      .grouped(8)
+      .map(_.mkString)
+      .mkString("\n")
 
     println(colorMapStr)
   }
